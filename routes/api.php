23ecb7923009331e23f7f1e5f61abeea9daad440
sadi-tanvir <htanvir.sadi@gmail.com>
2024-06-26 17:59:21 +0600

    feat: Add features for displaying waiver reports

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/welcome', function () {
+    return response()->json([
+        'message' => 'Welcome to the API',
+    ]);
+});

f06595b56c9d9a9172ec15e4fb97bd31bd28bbdf
Asma Akter <142030196+AsmaCSE@users.noreply.github.com>
2024-06-28 03:48:52 -0700

    update code

--- resources/views/Backend/BasicInfo/FeesSetting/AddFeeType.blade.php
+++ resources/views/Backend/BasicInfo/FeesSetting/AddFeeType.blade.php
@@ -173,17 +173,15 @@ Add Fee Type
                 </div>
             </div>
             {{-- Add Fee type modal form end --}}
-
-            {{-- search box --}}
-
-
-
         </div>
+
+         {{-- search box --}}
         <form id="searchForm" class="mb-0 grow" method="GET" action="{{ route('addFeeType.view', $schoolCode) }}">
             <input type="text" value="{{ request('search_types') }}" name="search_types" id="search_types"
                 class="bg-white border-0  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-4 block w-full"
                 placeholder="Search..." />
         </form>
+
         <button>
             <a href="{{ route('addFeeType.print', $schoolCode) }}" target="_blank"
                 class=" border-2 border-blue-600 focus:ring-4 focus:outline-none text-[#174093] focus:ring-red-300 font-medium rounded-lg text-sm px-5 ext-center flex items-center gap-3 py-3.5">

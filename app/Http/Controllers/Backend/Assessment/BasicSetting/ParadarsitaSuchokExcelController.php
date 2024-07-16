6610211e7185216abf16abc82e50d8c26205533c
sadi-tanvir <htanvir.sadi@gmail.com>
2024-07-16 15:33:27 +0600

    feat: add functionality to insertparadarshita suchok using excel sheet

--- app/Http/Controllers/Backend/Assessment/BasicSetting/ParadarsitaSuchokExcelController.php
+++ app/Http/Controllers/Backend/Assessment/BasicSetting/ParadarsitaSuchokExcelController.php
@@ -3,11 +3,94 @@
 namespace App\Http\Controllers\Backend\Assessment\BasicSetting;
 
 use App\Http\Controllers\Controller;
+use App\Models\AddClass;
+use App\Models\AddClassWiseSubject;
+use App\Models\ParadarsitaSuchok;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class ParadarsitaSuchokExcelController extends Controller
 {
-    public function ParadarsitaSuchokExcelView(){
-        return view("Backend.Assessment.BasicSetting.ParadarsitaSuchokExcel");
+    public function ParadarsitaSuchokExcelView(Request $request, $school_code)
+    {
+        $classes = AddClass::where('school_code', $school_code)
+            ->where('action', 'approved')
+            ->get();
+        return view("Backend.Assessment.BasicSetting.ParadarsitaSuchokExcel", compact('classes'));
+    }
+
+    // get class wise subjects
+    public function GetSubjects(Request $request, $school_code)
+    {
+        $class = $request->input("class_name");
+        $subjects = AddClassWiseSubject::where('school_code', $school_code)
+            ->where('action', 'approved')
+            ->where("class_name", $class)
+            ->get();
+        return response()->json([
+            "subjects" => $subjects
+        ]);
+    }
+
+    // download a blank excel file
+    public function DownloadBlankExcel()
+    {
+        $filePath = public_path('Assessment-add.xlsx');
+        return Response::download($filePath, 'Assessment-add.xlsx', [], 'inline');
+    }
+
+
+    // Upload Excel file
+    public function UploadExcelFile(Request $request, $school_code)
+    {
+        $class = $request->input("class");
+        $segment = $request->input("segment");
+        $subject = $request->input("subject");
+
+        if (!$request->hasFile('file') || !$class || !$segment || !$subject) {
+            return redirect()->back()->with('error', 'Provide All the necessary information');
+        }
+
+        $paradarshitaSuchoks = [];
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $file->move(storage_path('app/uploads'), $file->getClientOriginalName());
+            $filePath = storage_path('app/uploads') . '/' . $file->getClientOriginalName();
+            $paradarshitaSuchoks = $this->readExcel($filePath);
+        }
+
+        foreach ($paradarshitaSuchoks as $suchok) {
+            $paradarsitaSuchok = new ParadarsitaSuchok();
+            $paradarsitaSuchok->class = $class;
+            $paradarsitaSuchok->segment = $segment;
+            $paradarsitaSuchok->subject = $subject;
+            $paradarsitaSuchok->suchok_name = $suchok[0];
+            $paradarsitaSuchok->suchok_value = $suchok[1];
+            $paradarsitaSuchok->suchok_matra_rectengle = $suchok[2];
+            $paradarsitaSuchok->suchok_matra_circle = $suchok[3];
+            $paradarsitaSuchok->suchok_matra_triangle = $suchok[4];
+            $paradarsitaSuchok->save();
+        }
+
+        return redirect()->back()->with('success', 'Suchok Added Successfully.');
+    }
+
+
+    private function readExcel($filePath)
+    {
+        $data = [];
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+        $spreadsheet = $reader->load($filePath);
+        $worksheet = $spreadsheet->getActiveSheet();
+        foreach ($worksheet->getRowIterator() as $row) {
+            $rowData = [];
+            foreach ($row->getCellIterator() as $cell) {
+                $rowData[] = $cell->getValue();
+            }
+            $data[] = $rowData;
+        }
+        array_shift($data);
+        return $data;
     }
 }
--- resources/views/Backend/Assessment/BasicSetting/ParadarsitaSuchokExcel.blade.php
+++ resources/views/Backend/Assessment/BasicSetting/ParadarsitaSuchokExcel.blade.php
@@ -1,86 +1,120 @@
 @extends('Backend.app')
 @section('title')
-    Paradarsita Suchok Excel
+Paradarsita Suchok Excel
 @endsection
 
 
 @section('Dashboard')
-    <div class="w-full mx-auto p-5 space-y-10">
-        <div>
-            <h1 class="text-xl font-semibold">পারদর্শিতার সূচক গঠন</h1>
-        </div>
+@include('Message.message')
 
-        {{-- first section --}}
-        <div class="grid grid-cols-2">
-            <div class="">
-                <form action="{{ url('') }}" method="POST">
-                    @csrf
+<div>
+    @include('Shared.ContentHeader', ['title' => 'পারদর্শিতার সূচক গঠন'])
+    <form action="{{route("paradarsitaSuchokExcel.upload", $school_code)}}" enctype="multipart/form-data" method="POST">
+        @csrf
+        <div class="w-full mx-auto p-5 space-y-10">
+
+            {{-- first section --}}
+
+            <div class="grid grid-cols-2">
+                <div class="">
                     <div class="py-5 px-2 space-y-3 flex flex-col">
                         <div class="grid grid-cols-4 space-x-5 items-center">
                             <label for="class" class="ml-auto mb-2 text-sm font-medium text-gray-900 ">শ্রেণি:</label>
                             <select id="class" name="class"
-                                class="col-span-3 bg-gray-50 border border-gray-300 px-5 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1 block w-5/6">
-                                <option selected>Select</option>
-                                <option value="US">United States</option>
-                                <option value="CA">Canada</option>
-                                <option value="FR">France</option>
-                                <option value="DE">Germany</option>
+                                class="bg-gray-50  text-gray-900 text-sm rounded-lg  block w-full p-2.5 col-span-3">
+                                <option disabled selected>Select</option>
+                                @foreach ($classes as $class)
+                                    <option value="{{ $class->class_name }}">{{ $class->class_name }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="grid grid-cols-4 space-x-5 items-center">
                             <label for="class" class="ml-auto mb-2 text-sm font-medium text-gray-900 ">বিভাগ:</label>
-                            <select id="class" name="class"
-                                class="col-span-3 bg-gray-50 border border-gray-300 px-5 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1 block w-5/6">
-                                <option selected>Select</option>
-                                <option value="US">United States</option>
-                                <option value="CA">Canada</option>
-                                <option value="FR">France</option>
-                                <option value="DE">Germany</option>
+                            <select id="segment" name="segment"
+                                class="bg-gray-50  text-gray-900 text-sm rounded-lg  block w-full p-2.5 col-span-3">
+                                <option disabled selected>Select</option>
+                                <option value="PI">PI</option>
+                                <option value="BI">BI</option>
                             </select>
                         </div>
                     </div>
-                </form>
-            </div>
+                </div>
 
 
-            {{-- left section --}}
-            <div class="">
-                <form action="{{ url('') }}" method="POST">
-                    @csrf
+                {{-- left section --}}
+                <div class="">
                     <div class="py-5 px-2 space-y-3 flex flex-col">
                         <div class="grid grid-cols-4 space-x-5 items-center">
                             <label for="class" class="ml-auto mb-2 text-sm font-medium text-gray-900 ">বিষয়:</label>
-                            <select id="class" name="class"
-                                class="col-span-3 bg-gray-50 border border-gray-300 px-5 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1 block w-5/6">
-                                <option selected>Select</option>
-                                <option value="US">United States</option>
-                                <option value="CA">Canada</option>
-                                <option value="FR">France</option>
-                                <option value="DE">Germany</option>
+                            <select id="subject" name="subject"
+                                class="bg-gray-50  text-gray-900 text-sm rounded-lg  block w-full p-2.5 col-span-3">
+                                <option disabled selected>Select</option>
                             </select>
                         </div>
                         <div class="grid grid-cols-4 space-x-5 items-center">
-                            <label for="excel_file" class="ml-auto mb-2 text-sm font-medium text-gray-900 ">এক্সেল ফাইল :</label>
-
-                            <input
-                                class="col-span-3 w-5/6 block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none  dark:border-gray-600 "
+                            <label for="excel_file" class="ml-auto mb-2 text-sm font-medium text-gray-900 ">এক্সেল ফাইল
+                                :</label>
+                            <input name="file"
+                                class="col-span-3 w-full block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none  dark:border-gray-600 "
                                 id="excel_file" type="file">
-
                         </div>
                     </div>
-                </form>
-            </div>
-        </div>
+                </div>
 
-        <div class="w-full flex justify-center items-center gap-5">
-                <button type="button"
+            </div>
+
+            <div class="w-full flex justify-center items-center gap-5">
+                <!-- <form action="{{route("paradarsitaSuchokExcel.download", $school_code)}}" method="GET"> -->
+                <a href="{{route("paradarsitaSuchokExcel.download", $school_code)}}"
                     class="text-white bg-gradient-to-br from-red-600 to-red-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-28 py-2.5 text-center">
                     Blank Excel Download
-                </button>
-                <button type="button"
+                </a>
+                <!-- </form> -->
+                <button type="submit"
                     class="text-white bg-gradient-to-br from-blue-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-28 py-2.5 text-center">
                     Save
                 </button>
+            </div>
         </div>
-    </div>
+    </form>
+</div>
 @endsection
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const schoolCode = @json($school_code);
+        const classId = document.getElementById('class');
+        const subjectId = document.getElementById('subject');
+
+        // get subjects
+        classId.addEventListener("change", async (e) => {
+            try {
+                const res = await fetch(
+                    `/dashboard/assessment/basicSetting/paradarsitaSuchokExcel/getSubjects/${schoolCode}?class_name=${e.target.value}`
+                );
+                if (!res.ok) throw new Error('Network response was not ok');
+                const data = await res.json();
+                console.log('from suchok', data);
+                UpdateSubjectOption(data.subjects);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        })
+
+
+        // update section options
+        function UpdateSubjectOption(subjects) {
+            subjectId.innerHTML = "";
+            const defaultOption = document.createElement('option');
+            defaultOption.value = "Select";
+            defaultOption.textContent = "Select";
+            defaultOption.selected = true;
+            subjectId.appendChild(defaultOption);
+            subjects.forEach(subject => {
+                const subjectOption = document.createElement('option');
+                subjectOption.value = subject.subject_name;
+                subjectOption.textContent = subject.subject_name;
+                subjectId.appendChild(subjectOption);
+            });
+        };
+    })
+</script>
